114fcd61431979dcf0e5c4b9fde2cb9b99e96e90
vuthanhhai <tangiadinh125@gmail.com>
2016-08-10 14:49:43 +0700

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='https://www.facebook.com/vatlieuchiulua125' target='_blank'><img src='https://lh6.googleusercontent.com/lZe41XnE_LUKmTIT0XgJr5r0wVzDIvdYty_YrXb0FDA366PzbRQavOC_4DH-sJBbJMGkhPh-xRiHZOA=w1920-h979-rw' width='500px'/><br/>Xây lò công nghiệp chất lượng cao</a>. vơi cong ty Tân Gia Định. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='https://www.facebook.com/vatlieuchiulua125' target='_blank'><img src='https://drive.google.com/file/d/0B9toPYMXdkoAUmdLQzRMWkxBbE0/preview' width='500px'/><br/>Xây lò công nghiệp chất lượng cao</a>. vơi cong ty Tân Gia Định. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
